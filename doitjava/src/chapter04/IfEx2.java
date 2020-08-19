4c060ee6070c8d98a261fd59f8146b026a15e663
Leehyosang <hyosang.dev.gmail.com>
2020-08-19 15:57:56 +0900

    chapter04 - 조건문ex

---
+++ doitjava/src/chapter04/IfEx2.java
@@ -0,0 +1,29 @@
+package chapter04;
+
+public class IfEx2 {
+
+	public static void main(String[] args) {
+		int score = 75;
+
+		if (score >= 90) {
+			System.out.println("점수가 90보다 크거나 같습니다.");
+			System.out.println("등급은 A입니다.");
+		} else if (score >= 80) {
+			System.out.println("점수가 80보다 크거나 같습니다.");
+			System.out.println("등급은 B입니다.");
+		} else if (score >= 70) {
+			if (score >= 75) {
+				System.out.println("점수가 75보다 크거나 같습니다.");
+				System.out.println("등급은 C+입니다.");
+			} else {
+				System.out.println("점수가 70보다 크거나 같습니다.");
+				System.out.println("등급은 C입니다.");
+			}
+		} else {
+			System.out.println("등급은 F입니다.");
+		}
+
+		System.out.println("마지막 줄입니다.");
+	}
+
+}
