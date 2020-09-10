20ed2e1692637c50d05a459d13468a11923c859b
Leehyosang <hyosang.dev@gmail.com>
2020-09-10 16:19:58 +0900

    9-10

---
+++ doitjava/src/chapter05/CalcStatic.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+public class CalcStatic {
+	static double pi = 3.14159;	//정적 멤버
+	int num1 = 10;	//인스턴트 멤버
+	int num2 = 20;	//인스턴트 멤버
+	
+	static int plus(int x, int y) {	//정적 멤버
+		return x + y;
+	}
+	
+	static int minus(int x, int y) { //정적 멤버
+		return x - y;
+	}
+	static int multiply(int x, int y) { //인스턴스 멤버
+		return x * y;
+	}
+}
