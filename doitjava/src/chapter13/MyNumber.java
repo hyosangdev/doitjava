fc8642237183352b7292880da7472846c9c738a8
Leehyosang <hyosang.dev@gmail.com>
2020-09-25 15:56:12 +0900

    2020-09-25

---
+++ doitjava/src/chapter13/MyNumber.java
@@ -0,0 +1,6 @@
+package chapter13;
+
+@FunctionalInterface	//함수형 인터페이스, 메서드 하나이상 선언x
+public interface MyNumber {
+	int getMax(int num1, int num2);
+}
---
+++ doitjava/src/chapter13/TestMyNumber.java
@@ -0,0 +1,32 @@
+package chapter13;
+
+public class TestMyNumber {
+	public static void main(String[] args) {
+//		MyNumber max = (x, y) -> (x >= y) ? x : y;
+//		System.out.println(max.getMax(10, 20));
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		//클래스 기반의 객체 생성
+		MyNumber in = new ImplNumber();
+		int result = in.getMax(num1, num2);
+		System.out.println(result);
+		
+		//익명 구현 객체 생성
+		MyNumber in2 = new MyNumber() {
+			
+			@Override
+			public int getMax(int num1, int num2) {
+				return num1 >= num2 ? num1 : num2;
+			}
+		};
+		result = in.getMax(num1, num2);
+		System.out.println("in2: " + result);
+		
+		//람다식 사용
+		MyNumber in3 = (x, y) -> x >= y ? x : y;
+		result = in.getMax(num1, num2);
+		System.out.println("in3: " + result);
+	}
+}
